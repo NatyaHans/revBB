6b43214f3348cdbaa8b92ae12a4ffd4f8a6e0fe5
Natya Hans <nhans@gator4.ufhpc>
2019-05-28 00:02:25 -0400

    pushing repo with 1000 trees

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Project pipeline
+#
+# Step1: Simulate 100 birth death trees in R using package diversitree
+#  - saved in data_simtreebd
+# Step2: 
--- scripts/simtreebd.R
+++ scripts/simtreebd.R
@@ -3,9 +3,9 @@ library(diversitree)
 source("simulate.NotNullTree.R")
 pars<-c(0.1,0.01)
 taxa<-50
-sim<-100
+sim<-1000
 for(i in 1:sim){
 temp<-simulate.NotNullTree(pars,tree.bd,maxtaxa=taxa,extinct=FALSE)
 #temp<-tree.bd(pars, max.taxa=Inf, max.t=taxa, include.extinct=FALSE)
-write.nexus(temp,file = paste("simtreebd",i,sep="_"))
+write.nexus(temp,file = paste("simbd",i,".nex",sep="_"))
 }
