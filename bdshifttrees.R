1609ec177a62d1647763989c2e0b37774cee4a3f
Natya Hans <nhans@gator4.ufhpc>
2019-05-30 23:30:30 -0400

    updating the bdshift R script

--- bdshifttrees.R
+++ bdshifttrees.R
@@ -4,8 +4,10 @@ lambda<-c(0.5,0.1) # ancestral and current speciation rate vector
 mu<-c(0,0.01) # ancestral and current extinction rate vector
 frac<-c(1,1) # only rate shifts (no mass extinction)
 times<-c(0,0.3) # rate shift times
-sim<-100
-taxa<-50
+sim<-100 # number of trees
+taxa<-50 # taxa 
+
+#Note that taxa needs to be automated at some point.
 # Simulating trees with a fixed number of species having shifts in rate
 #
 tree<-sim.rateshift.taxa(n=taxa,numbsim = sim,lambda = lambda,mu=mu,frac = frac, times = times, complete = FALSE)
@@ -14,17 +16,3 @@ write.nexus(tree,file = "simbdtreelist.nex")
 
 
 
-
-# The fraction 0.6 of the extant species is included into the final tree
-# (the tree has n species AFTER sampling, extinct and
-# non-sampled lineages are not included):
-
-
-
-sim.bd.taxa
-sim.bd.age
-sim.bd.taxa.age
-sim.genespeciestree
-sim.genetree
-sim.gsa.taxa
-sim.rateshift.taxa
